7b548b816370067566ff09bf6d01948d95659b5b
bansalhimanshu0296 <bansalhimanshu0296@gmail.com>
2019-01-13 01:56:00 +0530

    Base Project for Pet Clinic Project

---
+++ src/main/java/himanshu/springframework/himanshupetclinic/HimanshuPetClinicProjectApplication.java
@@ -0,0 +1,14 @@
+package himanshu.springframework.himanshupetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HimanshuPetClinicProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HimanshuPetClinicProjectApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/himanshu/springframework/himanshupetclinic/HimanshuPetClinicProjectApplicationTests.java
@@ -0,0 +1,17 @@
+package himanshu.springframework.himanshupetclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HimanshuPetClinicProjectApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
