2b4a13778e8ca790e90c1e8b0d914eb4a9ac6579
bansalhimanshu0296 <bansalhimanshu0296@gmail.com>
2019-01-17 00:34:07 +0530

    Implemented Map Based Service. Closes #6.

---
+++ pet-clinic-data/src/main/java/himanshu/springframework/himanshupetclinic/services/map/OwnerServiceMapImpl.java
@@ -0,0 +1,35 @@
+package himanshu.springframework.himanshupetclinic.services.map;
+
+import himanshu.springframework.himanshupetclinic.model.Owner;
+import himanshu.springframework.himanshupetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class OwnerServiceMapImpl extends AbstractMapService<Owner,Long> implements CrudService<Owner,Long> {
+
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return super.save(object.getId(),object);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
+
