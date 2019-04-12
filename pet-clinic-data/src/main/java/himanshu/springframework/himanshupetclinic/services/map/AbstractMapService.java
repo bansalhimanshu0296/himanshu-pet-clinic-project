75ba70bf6d353a5dd5e24c80f5080bd5f9c7f480
bansalhimanshu0296 <bansalhimanshu0296@gmail.com>
2019-04-12 07:06:29 +0530

    Refactor Map impl to ID generator. Closes #23.

--- pet-clinic-data/src/main/java/himanshu/springframework/himanshupetclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/himanshu/springframework/himanshupetclinic/services/map/AbstractMapService.java
@@ -1,13 +1,12 @@
 package himanshu.springframework.himanshupetclinic.services.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import himanshu.springframework.himanshupetclinic.model.BaseEntity;
 
-public abstract class AbstractMapService<T,ID> {
+import java.util.*;
 
-    protected Map<ID,T> map=new HashMap<>();
+public abstract class AbstractMapService<T extends BaseEntity,ID extends Long> {
+
+    protected Map<Long,T> map=new HashMap<>();
 
     Set<T> findAll(){
         return new HashSet<>(map.values());
@@ -17,8 +16,17 @@ public abstract class AbstractMapService<T,ID> {
         return map.get(id);
     }
 
-    T save(ID id,T object){
-        map.put(id, object);
+    T save(T object){
+        if(object!=null){
+            if(object.getId()==null) {
+                object.setId(getNextKey());
+            }
+            map.put(object.getId(),object);
+        }
+        else
+        {
+            throw new RuntimeException("Object Cannot be null");
+        }
         return object;
     }
 
@@ -30,4 +38,13 @@ public abstract class AbstractMapService<T,ID> {
         map.entrySet().removeIf(entry->entry.getValue().equals(object));
     }
 
+    private Long getNextKey(){
+        Long nextid=null;
+        try{
+            nextid = Collections.max(map.keySet())+1;
+        }catch (NoSuchElementException e){
+            nextid = 1L;
+        }
+        return nextid;
+    }
 }
--- pet-clinic-data/src/main/java/himanshu/springframework/himanshupetclinic/services/map/PetServiceMapImpl.java
+++ pet-clinic-data/src/main/java/himanshu/springframework/himanshupetclinic/services/map/PetServiceMapImpl.java
@@ -21,7 +21,7 @@ public class PetServiceMapImpl extends AbstractMapService<Pet,Long> implements P
 
     @Override
     public Pet save(Pet object) {
-        return super.save(object.getId(),object);
+        return super.save(object);
     }
 
     @Override
